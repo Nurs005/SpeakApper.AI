092d5aedbc4252633750adbd71ff8fb84e47e0fd
Nurs005 <tenb1105@gmail.com>
2025-06-20 05:54:09 +0500

    Add paywall funcs

--- SpeakApper.AI/Application/SpeakApper_AIApp.swift
+++ SpeakApper.AI/Application/SpeakApper_AIApp.swift
@@ -5,30 +5,34 @@
 //  Created by Akmaral Ergesh on 25.01.2025.
 //
 
-import SwiftUI
 import Firebase
+import SwiftUI
 
 @main
 struct SpeakApper_AIApp: App {
     let dependencies = Dependencies()
     let coordinator = Coordinator()
-    
+
     init() {
         FirebaseApp.configure()
     }
-    
+
     var body: some Scene {
         WindowGroup {
-            CoordinatorView(coordinator: coordinator,
-                            dependencies: dependencies)
-            
-//                        RecordingDetailView(recording: Recording(
-//                            url: Bundle.main.url(forResource: "example-audio", withExtension: "m4a") ?? URL(fileURLWithPath: ""),
-//                            date: Date(),
-//                            sequence: 0,
-//                            transcription: "Это пример транскрипции"
-//                        ))
-            
+            CoordinatorView(
+                coordinator: coordinator,
+                dependencies: dependencies)
+            .task {
+                await dependencies.subscriptionManager.updatePurchasedProducts()
+            }
+
+            //                        RecordingDetailView(recording: Recording(
+            //                            url: Bundle.main.url(forResource: "example-audio", withExtension: "m4a") ?? URL(fileURLWithPath: ""),
+            //                            date: Date(),
+            //                            sequence: 0,
+            //                            transcription: "Это пример транскрипции"
+            //                        ))
+
         }
     }
 }
